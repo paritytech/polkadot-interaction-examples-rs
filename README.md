8e07298f1c8c32068f6aa53a1ecca558bc8239de
James Wilson <james@jsdw.me>
2021-09-08 12:44:10 +0100

    Add README and more comment adjusting

---
+++ README.md
@@ -0,0 +1,26 @@
+# Examples of interacting with a Polkadot node
+
+Some examples of using JSON RPC to interact with a Polkadot node, working up to manually building and submitting a balance transfer.
+
+To run these examples, first start up a local Polkadot node (which we'll be interacting with):
+
+```
+# Clone the polkadot repo:
+git clone https://github.com/paritytech/polkadot.git
+# This is the commit I used (the examples will likely break as master is updated):
+git checkout f3f83e3f9db049f981066b3a94fa17cad673299f
+# Start up a polkadot dev node on your machine:
+cargo run -- --tmp --dev
+```
+
+Once you have this node running, in another terminal, pick an example you'd like to run and run it like so:
+
+```
+cargo run --bin 01_basic
+```
+
+Note that the balance transfer example expects a fresh dev node (the transaction has a nonce which means it can't be executed more than once). Just restart the Polkadot node to get back to a fresh state.
+
+The examples are well commented, so check them out to find out more!
+
+One really useful tip for debugging what is happening is to visit https://polkadot.js.org/apps, and point it at your local node (click top left corner and in the "development" tab, point it to the node at `ws://127.0.0.1:9944`). Using this, you can see the balance transfer in example 05 take place, and compare other results with the actual node state.
--- src/bin/05_transfer_balance.rs
+++ src/bin/05_transfer_balance.rs
@@ -6,9 +6,15 @@ Run with:
 cargo run --bin 05_transfer_balance
 ```
 
-To transfer money from one account to another, we need to construst an extrinsic which ends up
-calling the `transfer` method in the Balances pallet (`substrate/frame/balances/src/lib.rs`).
-The transfer method signature, from that code, is this:
+An extrinsic consists of call information (what function are we calling from which pallet, and which
+arguments does that function want to be given) and optionally (but likely) also signature information
+(who is submitting the extrinsic, the signature itself, and extra information about where/how long is
+it valid for). All of this information is SCALE encoded in a particular format, converted to a hex string,
+and submitted via JSONRPC to action the extrinsic.
+
+Extrinsics are generated from code in the `substrate/frame` folder. For example, to transfer money from
+one account to another, we need to construst an extrinsic which ends up calling the `transfer` method
+in the Balances pallet (`substrate/frame/balances/src/lib.rs`), whose signature looks like:
 
 ```no_run
 pub fn transfer(
@@ -18,9 +24,10 @@ pub fn transfer(
 )
 ```
 
-The Macro applied on the pallet will generate a `Call` enum containing a `transfer` variant with
-those parameters (origin is known separately). We can see this by going to `substrate/frame/balances` and running
-`cargo doc --open`. It looks something like:
+There is a macro applied on these functions which will generate a `Call` enum that contains, amoung other
+variants, a `transfer` variant, whose arguments are identical to those seen in the function definition
+(ignoring origin, which is separate and known by way of the extrinsic signature). We can see this by going
+to `substrate/frame/balances` and running `cargo doc --open`. It looks something like:
 
 ```no_run
 pub enum Call<T: Config<I>, I: 'static = ()> {
@@ -33,26 +40,29 @@ pub enum Call<T: Config<I>, I: 'static = ()> {
 }
 ```
 
-However, there are more pallets than just Balances, so this `Call` enum is actually just one variant
-in an outer enum. For a Polkadot node, we can see this enum by running `cargo doc --open` in
-`polkadot/runtime/polkadot` and finding the outer `Call` enum there.
+However, there are more pallets than just Balances, so not only do we have this inner enum, which describes
+the method in the pallet you'd like to call and the data to provide to it, but we have an outer enum which
+contains this, and others, to describe all of the calls from every pallet. For a Polkadot node, we can see this
+enum by running `cargo doc --open` in `polkadot/runtime/polkadot` and finding the outer `Call` enum there.
 
-That all said, the runtime gives us back metadata (see 03_metadata) which, as of V14 (which is soon to be
-released) will contain all of the information needed to construct the right value to send off without
-having to inspect any code.
+So, if we were to construct one of these enum variants we'd have part of the information we want to submit
+(the signature details being the other part). However, we don't actually need to include all of `polkadot`
+in order to have access to this generated enum; we just need to encode data that will have the same encoded
+shape as the enum.
 
-An extrinsic is composed of the `Call` like data (to say "I want to call this function with these params",
-but is also prepended with either a `u8: 0` value if it's not signed, or signature information if it is.
+With that all said, a running node can hand us metadata (see the example 03_metadata) which, as of V14 (which
+is soon to be released) will actually contain all of the type information needed to construct a valid extrinsic
+without us needing to dig through substrate/polkadot types to figure out what the concrete type of things like
+<T::Lookup as StaticLookup>::Source` or `T::Balance` are (which we'd need to do if constructing a balance
+transfer call).
 
-To construct an extrinsic, we basically need to encode the correct series of bytes to send off. We can do
-that using the `Call` enums and such, or we can look at the metadata and manually piece values together that
-will encode to the same format at the actual types. We'll mostly do the latter.
+So, how can we actually construct a valid extrinsic from the upcoming V14 metadata then?
 
-So, step 1. What types does the call need? Well, with V14 metadata we'll se able to see the type info in
-our metadata bundle. Using some example typed metadata from `https://gist.github.com/ascjones/b76a5345930776ede61dd0f797792ed4`,
-let's see what we can find out about the call we're about to make..
+Step 1: Construct our call data in a format that encodes to the same as the Call enum variant we'd want.
+Using some example typed metadata from `https://gist.github.com/ascjones/b76a5345930776ede61dd0f797792ed4`,
+let's see what we can find out about a call to transfer some balance.
 
-First, the "transfer" call in the balances module looks a bit like:
+First, the "transfer" call in the "Balances" module looks a bit like this in the V14 metadata:
 
 ```
 {
@@ -183,25 +193,29 @@ can view type #144 with `jq '.[1].V14.types.types[144]' ~/the-metadata.json`):
 }
 ```
 
-This shows us the entire inner enum, and we can look at the types of the two params for `transfer` (#145 and #168)
-to dig further into it (`jq '.[1].V14.types.types[145]'` and `jq '.[1].V14.types.types[168]'`). If we do this, we'll
-see that our extrinsic should be comprised of the following properties (in order):
+This is describing the entire inner enum that's generated for the Balances pallet. We can look at the types
+of the two params for `transfer` (#145 and #168) to dig further into it (`jq '.[1].V14.types.types[145]'`
+and `jq '.[1].V14.types.types[168]'`). If we do this, we can surmize that our extrinsic should be comprised of
+the following properties (in order):
 
-- Outer call variant index; which pallet are we calling into (`5: u8`) (index of "Balances" pallet in result from 03_metadata)
-- Inner call enum variant index (`0; u8`) (index of "transfer" call in result from 03_metadata)
+- Outer call variant index; which pallet are we calling into. If we find the index of "Balances" pallet in the
+  result from 03_metadata, we can see that it's index 5, and that'll be a `u8` to mirror the variant tag).
+- Inner call enum variant index. We can see that the `transfer` call is the first variant from the metadata, so
+  that'll be index 0, also a `u8`.
 - A "MultiAddress", which we can dig into and see that it consists of:
-  - A variant index to say we want to provide an `AccoundId32` (`0: u8`)
-  - The actual address (`[u8; 32]`)
-  (But we'll just import and use the MultiAddress type, which encodes to the same as above)
-- A Balance, which is a `u128` (but compact encoded; see SCALE encoding docs for details)
+  - A variant index (which will be 0) to say we want to provide an `AccoundId32`
+  - The actual address (with the type `[u8; 32]`)
+  (But we'll just import and use the MultiAddress type for simplicity in our example, rather than manually
+  encoding something of the same shape)
+- A Balance, which is a `u128` (but compact encoded; see SCALE encoding docs for details on what this means).
 
-So, a type like `(u8, u8, u8, [u8; 32], u128)` will encode to the correct bytes to represent the call we want to make.
-If a call doesn't need to be signed, we can just prepend a `None` signature to it (`0; u8`). If it does need to be
-signed, we'll need to gather and sign some details, including our call data, and prepend this signature/validity information.
-We can also attach extra information alongside the signature.
+So, a type like `(u8, u8, u8, [u8; 32], u128)` will encode to the correct bytes to represent the call we want
+to make. If a call doesn't need to be signed, we can just prepend a `None` signature to it (`0; u8`). If it
+does need to be signed, we'll need to gather and sign some details, including our call data, and prepend this
+signature/validity information. We can also attach extra information alongside the signature.
 
-When that's obtained, we encode the data in a specific way, and then we can send it off to be executed. The code
-below runs through these steps.
+When that's obtained, we encode the data in a specific way, and then we can send it off to be executed. The
+following example constructs and sends off a "balances.transfer" extrinsic:
 */
 
 use std::str::FromStr;
@@ -217,18 +231,19 @@ use parity_scale_codec::{ Compact, Encode };
 async fn main() {
 
     // First, we need to know which pallet, and which call in the pallet, we're
-    // actually calling. This equates to seeing which index inthe arrays in the
+    // actually calling. This equates to seeing which index in the arrays in the
     // metadata the "Balances" pallet and then the "transfer" call are at, but for
     // simplicity I've just manually had a look and hard coded them here:
     let pallet_index: u8 = 5;
     let call_index: u8 = 0;
 
-    // The "transfer" call takes 2 arguments, which are as follows:
+    // The "transfer" call takes 2 arguments, which are as follows (if we wanted, we could
+    // avoid using `MultiAddress` and encode a 0 u8 and then the account ID, but for simplicity..)
     let address = MultiAddress::Id::<_,u32>(AccountKeyring::Bob.to_account_id());
     let balance = Compact::from(123456789012345u128);
 
     // We put the above data together and now we have something that will encode to the
-    // correct bytes for the "call" part of the extrinsic we'll submit:
+    // Same shape as the generated enum would have led to (variant indexes, then args):
     let call = (
         pallet_index,
         call_index,
@@ -257,7 +272,7 @@ async fn main() {
     // This information won't be included in our payload, but is it part of the data
     // that we'll sign, to help ensure that the TX is only valid in the right place.
     let additional = (
-        // This TX won't be valid if it's not executed on the expected runtime:
+        // This TX won't be valid if it's not executed on the expected runtime version:
         runtime_version.spec_version,
         runtime_version.transaction_version,
         // Genesis hash, so TX is only valid on the correct chain:
@@ -266,6 +281,8 @@ async fn main() {
         // "immortal", use the genesis hash here. If it's mortal, this block hash
         // should be equal to the block number provided in the Era information,
         // so that the signature can verify that we're looking at the expected block.
+        // (one thing that this can help prevent is your transaction executing on the
+        // wrong fork; same genesis hash but likely different block hash for mortal tx).
         genesis_hash,
     );
 
@@ -275,7 +292,7 @@ async fn main() {
         let full_unsigned_payload = (&call, &extra, &additional);
         let full_unsigned_payload_scale_bytes = full_unsigned_payload.encode();
 
-        // If payload logner than 256 bytes, we hash it and sign the hash instead:
+        // If payload is longer than 256 bytes, we hash it and sign the hash instead:
         if full_unsigned_payload_scale_bytes.len() > 256 {
             AccountKeyring::Alice.sign(&blake2_256(&full_unsigned_payload_scale_bytes)[..])
         } else {
@@ -284,7 +301,7 @@ async fn main() {
     };
 
     // This is the format of the signature part of the transaction. If we want to
-    // experiment with an unsigned transaction, we can set this to None::<()> instead.
+    // experiment with an unsigned transaction here, we can set this to None::<()> instead.
     let signature_to_encode = Some((
         // The account ID that's signing the payload:
         MultiAddress::Id::<_,u32>(AccountKeyring::Alice.to_account_id()),
@@ -306,6 +323,7 @@ async fn main() {
     println!("Submitting this payload: {}", payload_hex);
     let res = rpc_to_localhost("author_submitExtrinsic", [payload_hex]).await.unwrap();
 
+    // The result from this call is the hex value for the extrinsic hash.
     println!("{:?}", res);
 }
 
